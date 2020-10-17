4aba10529a0b3432b7e31fd69b162119ddd4d455
howdyAnkit <palankit35@gmail.com>
2020-10-17 18:06:57 +0530

    Code Push for State Components

--- React-TODO/App.js
+++ React-TODO/App.js
@@ -3,14 +3,23 @@ import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
 
-function App() {
-    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
     
-    return (
-        <div className="todo-list">
-            {todoItems}
-        </div>
-    )
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )    
+    }
 }
 
 export default App
