6506be05f40e1cd2ccb519de5d5829952d6b95be
howdyAnkit <palankit35@gmail.com>
2020-10-19 14:18:41 +0530

    React Cart 🛒

---
+++ React-Cart/App.js
@@ -0,0 +1,53 @@
+
+import React from "react"
+import randomcolor from "randomcolor"
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0,
+            color: ""
+        }
+        this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
+    }
+    
+    increment() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+    decrement() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count - 1
+            }
+        })
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.count !== this.state.count) {
+            const newColor = randomcolor()
+            this.setState({color: newColor})
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1 style={{color: this.state.color}}>{this.state.count}</h1>
+                <button onClick={this.increment}>
+                    Increment!
+                </button>
+                <button onClick={this.decrement}>
+                    Decrement!
+                </button>
+            </div>
+        )
+    }
+}
+
+export default App
