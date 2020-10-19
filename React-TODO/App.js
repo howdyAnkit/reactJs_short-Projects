6d38222dc73548fa35e6afd0408b6f74942ebe71
howdyAnkit <palankit35@gmail.com>
2020-10-19 12:44:17 +0530

    Adding Check property using props and state

--- React-TODO/App.js
+++ React-TODO/App.js
@@ -9,10 +9,25 @@ class App extends React.Component {
         this.state = {
             todos: todosData
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
     }
     
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
         
         return (
             <div className="todo-list">
--- React-TODO/todosData.js
+++ React-TODO/todosData.js
@@ -1,4 +1,3 @@
-//jshint esversion:6
 const todosData = [
     {
         id: 1,
